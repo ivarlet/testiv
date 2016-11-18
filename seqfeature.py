67ae30fad064b3b2744c53375e19412e54c95c54
Elodie Sanfins <elodie.sanfins@gmail.com>
2016-11-18 10:13:57 +0100

    SeqFeature_version1

---
+++ seqfeature.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from Bio import SeqFeature
+
+def get_location(feature):
+	feature_location = feature.location
+	return (feature.location.start,feature.location.end,feature.location.strand)
